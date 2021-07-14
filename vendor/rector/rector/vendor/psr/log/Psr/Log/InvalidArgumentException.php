a58d0eb04dccf2df91e2c3e6ac7aaf130204dec1
Tomas Votruba <tomas.vot@gmail.com>
2021-07-14 16:13:40 +0200

    remove vendor

--- vendor/rector/rector/vendor/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace RectorPrefix20210710\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
--- vendor/rector/rector/vendor/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-class UnhandledMatchError extends \Error
-{
-}
-\class_alias('RectorPrefix20210710\\UnhandledMatchError', 'UnhandledMatchError', \false);
